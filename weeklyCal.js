e3ee7fd3c245dfac36162e2f318f7bee2f68f684
Kelsey Ando <kma3a@virginia.edu>
2015-03-17 16:25:30 -0400

    adding the change about viewing based on the day

--- weeklyCal.js
+++ weeklyCal.js
@@ -13,4 +13,21 @@ function Calender() {
 
 Calender.prototype.addEvent = function(event, day) {
 	this.weekEvents[day].push(event);
-}}
+}
+
+Calender.prototype.findEventByName = function(eventName) {
+	var foundEvent = false;
+	for (var day in this.weekEvents) {
+		this.weekEvents[day].forEach(function(event) {
+			if(event.name === eventName) {
+				return foundEvent = event;
+			}
+		}
+	}
+	return foundEvent;
+}
+Calender.prototype.viewEventsOnDay = function(day) {
+	this.weekEvents[day].forEach(function(event) {
+		event.viewEvent();
+	})	
+}
