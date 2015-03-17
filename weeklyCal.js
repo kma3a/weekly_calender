ef769052141a5fbf895faeef08f67d1b8d1487cd
Kelsey Ando <kma3a@virginia.edu>
2015-03-17 15:50:36 -0400

    creating object for storing the calender and the object to store the weeks in

--- weeklyCal.js
+++ weeklyCal.js
@@ -1,3 +1,8 @@
 function Event(name) {
 	this.name = name;
 }
+
+function calender() {
+	this.weekEvents= { monday:[], tuesday: [], wednesday: [], thursday: [], friday: [], saturday: [], sunday: []}
+}
+
